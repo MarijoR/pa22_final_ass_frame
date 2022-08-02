ee4431514a77269df3fb1a5216309ec6edd56858
Marijo Radman <marijo.radman@gmail.com>
2022-08-02 22:35:16 +0100

    test & mock update

--- pallets/template2/src/mock.rs
+++ pallets/template2/src/mock.rs
@@ -1,59 +1,20 @@
-use crate as pallet_template;
-use frame_support::traits::{ConstU16, ConstU64};
-use frame_system as system;
-use sp_core::H256;
-use sp_runtime::{
-	testing::Header,
-	traits::{BlakeTwo256, IdentityLookup},
-};
+use crate::{mock::*, Error};
+use frame_support::{assert_noop, assert_ok};
 
-type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
-type Block = frame_system::mocking::MockBlock<Test>;
-
-// Configure a mock runtime to test the pallet.
-frame_support::construct_runtime!(
-	pub enum Test where
-		Block = Block,
-		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic,
-	{
-		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-		TemplateModule: pallet_template::{Pallet, Call, Storage, Event<T>},
-	}
-);
-
-impl system::Config for Test {
-	type BaseCallFilter = frame_support::traits::Everything;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ();
-	type Origin = Origin;
-	type Call = Call;
-	type Index = u64;
-	type BlockNumber = u64;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type Event = Event;
-	type BlockHashCount = ConstU64<250>;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = ();
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = ConstU16<42>;
-	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
+#[test]
+fn can_create_claim() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		assert_ok!(TemplateModule::create_claim(Origin::signed(1), vec![1,2,3,4]));
+	});
 }
 
-impl pallet_template::Config for Test {
-	type Event = Event;
-}
+#[test]
+fn should_error_if_claim_rewrite_attempted() {
+	new_test_ext().execute_with(|| {
+		let proof = vec![1,2,3,4];
+		assert_ok!(TemplateModule::create_claim(Origin::signed(1), proof.clone()));
 
-// Build genesis storage according to the mock runtime.
-pub fn new_test_ext() -> sp_io::TestExternalities {
-	system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+		assert_noop!(TemplateModule::create_claim(Origin::signed(1), proof.clone()), Error::<Test>::ProofAlreadyClaimed);
+	})
 }
